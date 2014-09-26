033e8244d4f52503c26feaa9c75a3a27730a4933
Yuqing Chen <yuqingchen@ensoftcorp.com>
2014-09-26 17:58:29 -0500

    removed generated files

--- BannerAdListener/gen/com/google/example/gms/ads/banneradlistener/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.google.example.gms.ads.banneradlistener;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
